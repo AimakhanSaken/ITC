4a67946263fbe2f0ac103d34c26f6693b1e2f9e7
AimakhanSaken <aimakhansaken90@gmail.com>
2022-06-09 13:58:57 +0600

    zz

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    $('.burger__top_nav').click(function(event){
-        $('.burger__top_nav,.top_nav__menu').toggleClass('active');
-        $('.body').toggleClass('lock');
-
-    });
+$(document).ready(function(){ 
+    $('.burger__top_nav').click(function(event){ 
+        $('.burger__top_nav,.top_nav__menu').toggleClass('active'); 
+        $('.body').toggleClass('lock'); 
+ 
+    }); 
 });
