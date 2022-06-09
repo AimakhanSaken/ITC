6076d030239c254032c846b4c0e56eea4daea230
AimakhanSaken <aimakhansaken90@gmail.com>
2022-06-09 13:49:05 +0600

    adaptive

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.burger__top_nav').click(function(event){
+        $('.burger__top_nav,.top_nav__menu').toggleClass('active');
+        $('.body').toggleClass('lock');
+
+    });
+});
